92d8a530a98c8291545d5e20c686873cde6cacae
Nitya-sigadapu <mems240005048@iiti.ac.in>
2026-06-17 19:54:30 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "nitya3008",
+  password: process.env.DB_PASSWORD ||,
   database: process.env.DB_NAME || "campus_olx",
   port: process.env.DB_PORT || (process.env.DB_HOST ? 4000 : 3306)
 };
